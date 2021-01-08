c53dbbf85ef3524d21dd4a47a5aa4b2d766818e7
jin_tonic <kiyo254@naver.com>
2021-01-08 14:14:12 +0900

    jin_urls_changed

--- jin_tonic/urls.py
+++ jin_tonic/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('projects/', include('projectapp.urls')),
     path('subscribes/', include('subscribeapp.urls')),
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.base.MEDIA_URL, document_root = settings.base.MEDIA_ROOT)
