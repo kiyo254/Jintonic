633be436fdfef12d5d6e470313bee309b0d44142
jin_tonic <kiyo254@naver.com>
2021-01-08 11:01:25 +0900

    Devide local and deploy

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jin_tonic.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jin_tonic.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
